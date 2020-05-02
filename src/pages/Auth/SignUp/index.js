03e9f39eb49372df2857c7959067c8ae096bd24c
fellipebastos <fellipebastoswm@gmail.com>
2020-05-02 17:09:41 -0300

    add unform lib

--- src/pages/Auth/SignUp/index.js
+++ src/pages/Auth/SignUp/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Form } from '@unform/web';
 
 import olistImg from '../../../assets/logo-olist.png';
-import mercadoLivreImg from '../../../assets/logo-mercadolivre.png';
 
 import Card from '../../../components/Card';
 import Button from '../../../components/Button';
+import Input from '../../../components/Input';
 
 import { Container, Info } from './styles';
+
+const handleSubmit = (data) => {
+  console.log(data);
+};
 
 export default function SignUp() {
   return (
     <Container>
       <Card>
-        <form>
+        <Form onSubmit={handleSubmit}>
           <Info>
             Conexão <img src={olistImg} alt="Conexão Olist" /> efetuada com
             sucesso
@@ -20,30 +25,31 @@ export default function SignUp() {
 
           <h1>Cadastro de Dados</h1>
 
-          <label htmlFor="name">Nome</label>
-          <input id="name" name="name" value="" placeholder="Digite seu nome" />
+          <Input
+            id="name"
+            name="name"
+            label="Nome"
+            placeholder="Digite seu nome"
+          />
 
-          <label htmlFor="last_name">Sobrenome</label>
-          <input
+          <Input
             id="last_name"
             name="last_name"
-            value=""
+            label="Sobrenome"
             placeholder="Digite seu sobrenome"
           />
 
-          <label htmlFor="email">Email</label>
-          <input
+          <Input
             id="email"
             name="email"
-            value=""
+            label="Email"
             placeholder="Digite seu email"
           />
 
-          <label htmlFor="whatsapp">Whatsapp</label>
-          <input
+          <Input
             id="whatsapp"
             name="whatsapp"
-            value=""
+            label="Whatsapp"
             placeholder="Digite seu celular"
           />
 
@@ -53,7 +59,7 @@ export default function SignUp() {
           </p>
 
           <Button type="submit">Enviar</Button>
-        </form>
+        </Form>
       </Card>
     </Container>
   );
